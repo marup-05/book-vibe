2fc085603cb169745f5c72ef308668c70d831492
marup-05 <rahmanmarup05@gmail.com>
2026-09-22 13:15:16 +0600

    env added

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -5,9 +5,17 @@ import { Ibooks } from "@/types/books.type";
 import BookCard from "@/components/shared/BookCard";
 
 const getBooks = async () => {
-  const response = await fetch("http://localhost:3000//booksData.json");
+  try{
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/booksData.json`,
+  );
   const data = await response.json();
   return data;
+}catch(error){
+  console.error("Eror Fetching book data", error);
+  return [];
+}
+
 };
 
 const Books = async () => {
--- src/components/homepage/Books.tsx
+++ src/components/homepage/Books.tsx
@@ -4,9 +4,17 @@ import BookCard from "../shared/BookCard";
 import { Ibooks } from "@/types/books.type";
 
 const getBooks = async () => {
-  const response = await fetch("http://localhost:3000//booksData.json");
+  try{
+ const response = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/booksData.json`,
+  );
   const data = await response.json();
   return data;
+}catch(error){
+  console.error("Eror Fetching book data", error);
+  return [];
+}
+
 };
 
 const Books = async () => {
